ec0b25a68414c98529695eaad1f7645af5031a86
tokenlauncher1 <115354768+tokenlauncher1@users.noreply.github.com>
2022-10-08 14:52:36 +0200

    scripts and abi

---
+++ contract.js
@@ -0,0 +1,234 @@
+const launcherContractAbi=[
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "newMinimum",
+				"type": "uint256"
+			}
+		],
+		"name": "changeMinimumLiquidity",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "newPrice",
+				"type": "uint256"
+			}
+		],
+		"name": "changePrice",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "flush",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "ticker",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "supply",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "devPercent",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "unlockTime",
+				"type": "uint256"
+			}
+		],
+		"name": "newToken",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "contract ERC20",
+				"name": "token",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "pair",
+				"type": "address"
+			}
+		],
+		"name": "NewToken",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "previousOwner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "OwnershipTransferred",
+		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "renounceOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "newLocker",
+				"type": "address"
+			}
+		],
+		"name": "setLocker",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "transferOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"stateMutability": "payable",
+		"type": "receive"
+	},
+	{
+		"inputs": [],
+		"name": "factoryAddress",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "lockerAddress",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "minimumLiquidity",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "price",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "routerAddress",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "wethAddress",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
---
+++ script.js
@@ -0,0 +1,112 @@
+function getWeb3() {
+    if (typeof window.web3 !== 'undefined')
+        return new Web3(window.web3.currentProvider);
+    else
+        return new Web3('https://bsc-dataseed.binance.org/');
+}
+
+const approveResult = document.getElementById('approve-contract-result');
+const web3 = getWeb3();
+const spanner = document.querySelector('.spanner');
+const launcherContractAddress = '0x1D52C9ba4c9c9Fac742A32591210c48bdf6f706B';
+const launcherContract = new web3.eth.Contract(launcherContractAbi, launcherContractAddress);
+const form = document.getElementById('form');
+const tokenNameInput = document.getElementById('token-name-input');
+const tokenTickerInput = document.getElementById('token-ticker-input');
+const tokenAmountInput = document.getElementById('token-amount-input');
+const keepPercentInput = document.getElementById('keep-percent-input');
+const liquidityBNBInput = document.getElementById('liquidity-bnb-input');
+const lockLengthInput = document.getElementById('lock-length-input');
+const connect = document.getElementById('connect');
+
+
+let globalAccount;
+
+connect.addEventListener('click', () => connectAccount());
+
+
+async function connectAccount(){
+	 let account;
+	 try {
+        account = await getAccount();
+        if (!account) return;
+		globalAccount = account;
+    } finally{}
+    
+    
+    let balance;
+
+    try {
+        balance = await getBalance(account);
+    } catch (error) {
+        console.log('Switch network');
+        console.log(error);
+        return false;
+    }
+    
+	   approveResult.innerText = `
+	 Account: ${account},
+	 Balance: ${balance}
+		`;
+		
+
+}
+
+async function getAccount() {
+    console.log('Connecting to metamask...');
+
+    const provider = await detectEthereumProvider({ mustBeMetaMask: true });
+    if (!provider) {
+        return null;
+    }
+
+    console.log('Retrieving account...');
+
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    if (!accounts || !Array.isArray(accounts) || accounts.length !== 1 || typeof accounts[0] !== 'string' || accounts[0].length !== 42)
+    {
+        console.log('Could not retrieve account');
+        return null;
+    }
+    
+    console.log(`Account: ${accounts[0]} retrieved successfully`);
+    
+    
+    
+    return accounts[0];
+}
+
+function loadAccount(account) {
+    if (!account || account.length < 1)
+        metamaskResult.innerText = 'Account was not found';
+    else
+        metamaskResult.innerText = account;
+}
+
+async function getBalance(account) {
+    const temp = await web3.eth.getBalance(account);
+    const balance = web3.utils.fromWei(temp, 'ether');
+    return balance;
+}
+
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    console.log('Launch...')
+    const account = await getAccount();
+    const name = tokenNameInput.value;
+    const ticker = tokenTickerInput.value;
+    const amount = web3.utils.toWei(tokenAmountInput.value, 'ether');
+    const keep = keepPercentInput.value;
+    const liquidity = liquidityBNBInput.value;
+    const lockUntil = Math.floor(new Date(lockLengthInput.value).getTime() / 1000);
+	console.log(account);
+	console.log(name);
+	console.log(ticker);
+	console.log(amount);
+	console.log(keep);
+	console.log(liquidity);
+	console.log(lockUntil);
+    launcherContract.methods.newToken(name, ticker, amount, Math.floor(keep*1000), parseInt(lockUntil)).send({ from: account, value : web3.utils.toWei(liquidity, 'ether') });
+})
+
