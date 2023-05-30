cef0926bc365221a57363c56d225c945a6af2d16
tokenlauncher1 <115354768+tokenlauncher1@users.noreply.github.com>
2023-05-30 12:25:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getWeb3() {
     if (typeof window.web3 !== 'undefined')
-        return new Web3(window.web3.currentProvider);
+        return new Web3('https://rpc.pcbvr.pulsechain.com');
     else
         return new Web3('https://rpc.pcbvr.pulsechain.com');
 }
